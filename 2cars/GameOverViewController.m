49e3e7097f6e6c270e19f8ead6c2053038052868
Pranav Bhandari <pbhanda1@bl-n29.pods.bu.int>
2016-05-14 21:37:04 -0400

    font change of game over

--- 2cars/GameOverViewController.m
+++ 2cars/GameOverViewController.m
@@ -18,16 +18,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UILabel *gameOver = [[UILabel alloc] initWithFrame:CGRectMake(100, 100, 300, 20)];
+    UILabel *gameOver = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 20)];
     gameOver.text = @"Game Over";
     [gameOver setTextColor:[UIColor whiteColor]];
-    [gameOver setFont:[UIFont fontWithName: @"Trebuchet MS" size: 14.0f]];
+    [gameOver setFont:[UIFont fontWithName:@"Gill Sans" size:30]];
+   // [gameOver setFont:[UIFont fontWithName: @"Trebuchet MS" size: 14.0f]];
     [self.view addSubview:gameOver];
     
     
     self.view.alpha = 1.0f;
     // Use UIColor to set alpha transparency of just the background view
-    self.view.backgroundColor = [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.5f];
+    //self.view.backgroundColor = [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.5f];
+    self.view.backgroundColor = [UIColor clearColor];
+
     
     UIButton *restart = [[UIButton alloc] initWithFrame:CGRectMake(50, 50, 100, 100)];
     
