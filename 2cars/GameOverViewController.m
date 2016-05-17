1c377b691dade5bbab1d73ab84e3c869d90eebf5
Pranav Bhandari <Pranav@pranavs-mac.local>
2016-05-17 02:04:04 -0400

    added game over screen background image

--- 2cars/GameOverViewController.m
+++ 2cars/GameOverViewController.m
@@ -9,7 +9,6 @@
 #import "GameOverViewController.h"
 
 
-
 @interface GameOverViewController ()
 
 @end
@@ -18,14 +17,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UILabel *gameOver = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 20)];
+  //  self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"car.png"]];
+    self.view.backgroundColor = [[UIColor colorWithPatternImage:[UIImage imageNamed:@"bg1.png"]] colorWithAlphaComponent:0.5];
+    CGRect screenRect;
+    CGFloat screenWidth;
+    CGFloat screenHeight;
+    
+    screenRect = [[UIScreen mainScreen] bounds];
+    screenWidth = screenRect.size.width;
+    screenHeight = screenRect.size.height;
+    
+    
+  //  UILabel *gameOver = [[UILabel alloc] initWithFrame:CGRectMake(10, 10, 300, 20)];
+      UILabel *gameOver = [[UILabel alloc] initWithFrame:CGRectMake(screenWidth/2 - 60, 200, 300, 50)];
     gameOver.text = @"Game Over";
     [gameOver setTextColor:[UIColor whiteColor]];
     [gameOver setFont:[UIFont fontWithName:@"Gill Sans" size:30]];
    // [gameOver setFont:[UIFont fontWithName: @"Trebuchet MS" size: 14.0f]];
     [self.view addSubview:gameOver];
     
-    UILabel *myScore = [[UILabel alloc] initWithFrame:CGRectMake(10, 40, 300, 20)];
+   // UILabel *myScore = [[UILabel alloc] initWithFrame:CGRectMake(10, 40, 300, 20)];
+    UILabel *myScore = [[UILabel alloc] initWithFrame:CGRectMake(screenWidth/2 - 45, 250, 300, 50)];
     NSInteger myscr = [[NSUserDefaults standardUserDefaults] integerForKey:@"Score"];
     [myScore setText:[NSString stringWithFormat:@"Score : %ld",(long)myscr]];
     [myScore setTextColor:[UIColor whiteColor]];
@@ -33,7 +45,8 @@
     // [gameOver setFont:[UIFont fontWithName: @"Trebuchet MS" size: 14.0f]];
     [self.view addSubview:myScore];
     
-    UILabel *highScore = [[UILabel alloc] initWithFrame:CGRectMake(10, 70, 300, 20)];
+   // UILabel *highScore = [[UILabel alloc] initWithFrame:CGRectMake(10, 70, 300, 20)];
+     UILabel *highScore = [[UILabel alloc] initWithFrame:CGRectMake(screenWidth/2 - 65, 300, 300, 50)];
     NSInteger hiscr = [[NSUserDefaults standardUserDefaults] integerForKey:@"HighScore"];
     [highScore setText:[NSString stringWithFormat:@"High Score : %ld",(long)hiscr]];
     [highScore setTextColor:[UIColor whiteColor]];
@@ -43,7 +56,7 @@
     
     self.view.alpha = 1.0f;
     // Use UIColor to set alpha transparency of just the background view
-    self.view.backgroundColor = [UIColor colorWithRed:50.0f green:50.0f blue:50.0f alpha:0.5f];
+  //  self.view.backgroundColor = [UIColor colorWithRed:50.0f green:50.0f blue:50.0f ];
 
 //    UIButton *restart = [[UIButton alloc] initWithFrame:CGRectMake(50, 50, 100, 100)];
 //    
